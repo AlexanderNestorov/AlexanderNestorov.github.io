e2896353f67a6b1c0dfb40331c37888ba26231d0
Alexander Nestorov <jak.racer@gmail.com>
2022-03-05 15:30:48 +0200

    Connecting to local server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProductService} from "./services/product/product.service";
+import {Product} from "./models/product/Product";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'myApp';
+export class AppComponent implements OnInit {
+  title = 'grocery-shop';
+
+  products?: Product[];
+
+  constructor(private productService: ProductService) {
+  }
+
+  ngOnInit() {
+    this.getAllProducts();
+  }
+
+  public getAllProducts(): void {
+    this.productService.getAllProducts().subscribe(
+      (response: Product[]) => {
+        this.products = response;
+        console.log(response);
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    )
+  }
+
 }
